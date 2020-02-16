8f65cafb39f94c06223000478197e91dea414b2d
Tomosuke <orenoti@gmail.com>
2020-02-16 13:10:09 +0900

    Add Generamba template.

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let vc = UIViewController()
+        vc.present(vc, animated: true, completion: nil)
         return true
     }
 }
---
+++ Presentation/Presentation.h
@@ -0,0 +1,18 @@
+//
+//  Presentation.h
+//  Presentation
+//
+//  Created by Tomosuke Okada on 2020/02/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Presentation.
+FOUNDATION_EXPORT double PresentationVersionNumber;
+
+//! Project version string for Presentation.
+FOUNDATION_EXPORT const unsigned char PresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Presentation/PublicHeader.h>
+
+
