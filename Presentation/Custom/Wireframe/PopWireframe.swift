c92f6f11643d8f44fb2316b8f2d1860ca0de8ece
Tomosuke <orenoti@gmail.com>
2020-04-18 20:45:12 +0900

    Add PopWireframe.

--- Presentation/Custom/Wireframe/PopWireframe.swift
+++ Presentation/Custom/Wireframe/PopWireframe.swift
@@ -5,4 +5,18 @@
 //  Created by Tomosuke Okada on 2020/04/18.
 //
 
-import Foundation
+import UIKit
+
+protocol PopWireframe: AnyObject {
+
+    var viewController: UIViewController? { get }
+
+    func pop()
+}
+
+extension PopWireframe {
+
+    func pop() {
+        self.viewController?.navigationController?.popViewController(animated: true)
+    }
+}
