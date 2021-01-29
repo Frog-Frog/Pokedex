45c95714307f4f6f123ec6f9b0db9131efbf1690
Tomosuke <orenoti@gmail.com>
2021-01-30 01:43:20 +0900

    Add DataStore layer.

---
+++ DataStore/Entity/API/Request/ItemList/ItemListAPIRequest.swift
@@ -0,0 +1,20 @@
+//
+//  ItemListAPIRequest.swift
+//  DataStore
+//
+//  Created by Tomosuke Okada on 2021/01/30.
+//
+
+import Foundation
+
+struct ItemListAPIRequest: PokeAPIRequestable {
+
+    var path: String {
+        return "item"
+    }
+
+    let offset: Int = 0
+
+    // ページネーション実装するの面倒なので全件取得
+    let limit: Int = 954
+}
