32cb224a817d663a73d07e37f561b44b18c628ac
Tomosuke Okada <orenoti@gmail.com>
2021-09-05 15:53:06 +0900

    Add PokeAPIDataStore Tests.

--- PokedexTests/DataStore/DataStore/PokeAPI/PokeAPIDataStoreTests.swift
+++ PokedexTests/DataStore/DataStore/PokeAPI/PokeAPIDataStoreTests.swift
@@ -22,4 +22,46 @@ final class PokeAPIDataStoreTests: XCTestCase {
         self.dataStoreMock = APIDataStoreMock()
         self.pokeAPIDataStore = PokeAPIDataStoreImpl(dataStore: self.dataStoreMock)
     }
+
+    func test_success() {
+        self.dataStoreMock.requestHandler = { _, completion in
+            completion(.success(JsonParser.data(from: "PokemonList")))
+        }
+        self.pokeAPIDataStore.request(PokemonListAPIRequest()) { (result: Result<PokemonListResponse, Error>) in
+            switch result {
+            case .success:
+                return
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+    }
+
+    func test_api_failure() {
+        self.dataStoreMock.requestHandler = { _, completion in
+            completion(.failure(TestError.stub))
+        }
+        self.pokeAPIDataStore.request(PokemonListAPIRequest()) { (result: Result<PokemonListResponse, Error>) in
+            switch result {
+            case .success:
+                XCTFail("Don't throw network Error")
+            case .failure:
+                return
+            }
+        }
+    }
+
+    func test_parse_failure() {
+        self.dataStoreMock.requestHandler = { _, completion in
+            completion(.success(JsonParser.data(from: "PokemonDetail_Bulbasaur")))
+        }
+        self.pokeAPIDataStore.request(PokemonListAPIRequest()) { (result: Result<PokemonListResponse, Error>) in
+            switch result {
+            case .success:
+                XCTFail("Don't throw parse Error")
+            case .failure:
+                return
+            }
+        }
+    }
 }
